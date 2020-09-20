6b81e22cb686bf04e9672ce2565699c6241f84cb
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-20 15:36:51 +0100

    footer component completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/pages/Footer/Footer";
 import Home from "./components/pages/HomePage/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
